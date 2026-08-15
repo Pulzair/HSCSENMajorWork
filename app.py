66bebe3fb830f8cf570eb7564118d524d9dfe9bc
Pulzair <dcox2795@gmail.com>
2026-08-15 19:40:34 +1000

    404 handler

--- app.py
+++ app.py
@@ -38,6 +38,11 @@ def forbidden(error):
     return render_template("403.html"), 403
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html"), 404
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
